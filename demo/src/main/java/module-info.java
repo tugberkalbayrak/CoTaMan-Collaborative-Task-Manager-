35a0288e622be903c141d8285b6c76e4484e4cbb
Kaan Emre Öztürk <kaanemre2006@gmail.com>
2025-12-12 11:36:27 +0300

    databse ile alakalı

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -2,7 +2,13 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jsoup;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.core;
 
     opens com.example to javafx.fxml;
     exports com.example;
+
+    exports com.cotaman.database;
+    opens com.cotaman.database to org.mongodb.bson;
 }
